45faec56a31eb2768b03127fbc5e0af1f1f80b60
pooranjoyb <pooranjoyb2016@gmail.com>
2023-12-29 21:02:48 +0530

    Removed Persist & Async storage

--- utils/redux/store.ts
+++ utils/redux/store.ts
@@ -1,25 +1,12 @@
 
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './userSlice';
-import { persistStore, persistReducer } from 'redux-persist';
-import AsyncStorage from '@react-native-async-storage/async-storage'; 
-
-const persistConfig = {
-    key: 'root',
-    storage: AsyncStorage, 
-    whitelist: ['user']
-  };
-
-  const persistedReducer = persistReducer(persistConfig, userReducer);
 
 export const store = configureStore({
   reducer: {
-    user: persistedReducer,
-  },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-    immutableCheck: false,
-    serializableCheck: false,
-  })
+    user: userReducer,
+  }
 });
 
-export const persistor = persistStore(store);
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
