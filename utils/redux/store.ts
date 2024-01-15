0cebd797c97ba6a9c0d421df795211c65b40754c
Kamal <dragkamal71@gmail.com>
2024-01-15 19:39:25 +0530

    FIx : Persistent State of User Authentication

--- utils/redux/store.ts
+++ utils/redux/store.ts
@@ -1,11 +1,27 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './userSlice';
+import { persistStore, persistReducer } from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage'; 
+
+const persistConfig = {
+    key: 'root',
+    storage: AsyncStorage, 
+    whitelist: ['user']
+  };
+
+  const persistedReducer = persistReducer(persistConfig, userReducer);
 
 export const store = configureStore({
   reducer: {
-    user: userReducer,
-  }
-});
+    user: persistedReducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    immutableCheck: false,
+    serializableCheck: false,
+  })
+})
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
