dc7669ce556ce4fcf9c5a29cf1e22761b33cc3c3
Kamal <dragkamal71@gmail.com>
2024-01-16 10:47:31 +0530

    FIx : Persistent State of User Authentication

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Provider } from 'react-redux';
-import { store } from './utils/redux/store';
+import { persistor, store } from './utils/redux/store';
 
 import Signup from './Components/Signup';
 import Home from './Components/Home';
@@ -11,12 +11,14 @@ import Login from './Components/Login';
 import Profile from './Components/Profile';
 import StoriesList from './Components/StoriesList';
 import Story from './Components/Stories';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <Provider store={store}>
+      <PersistGate persistor={persistor}>
         <NavigationContainer>
           <Stack.Navigator initialRouteName="Home">
             <Stack.Screen name="Home" component={Home} />
@@ -28,6 +30,7 @@ export default function App() {
           </Stack.Navigator>
           <StatusBar style="auto" />
         </NavigationContainer>
+        </PersistGate>
     </Provider>
   );
 }
