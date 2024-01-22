0af9dae8f495feee8f34d5ca77a5b3d161cc052e
Mitali Raj <mitali.btmtcs22215@nfsu.ac.in>
2024-01-22 18:20:11 +0530

    chnages made to signup

--- Components/Signup.tsx
+++ Components/Signup.tsx
@@ -1,132 +1,218 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, Pressable, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import { account, ID } from '../utils/appwrite/service';
-import { showMessage } from "react-native-flash-message";
+import { showMessage } from 'react-native-flash-message';
 import FlashMessage from 'react-native-flash-message';
 import { useNavigation } from '@react-navigation/native';
 
 const Signup = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [age, setAge] = useState('');
+  const [bio, setBio] = useState('');
+  const [firstname, setFirstName] = useState('');
+  const [middlename, setMiddleName] = useState('');
+  const [lastname, setLastName] = useState('');
+  const [interests, setInterests] = useState('');
+  const [favoriteGenres, setFavoriteGenres] = useState('');
   const navigation = useNavigation();
 
   async function handleSignup() {
     try {
-
-      await account.create(ID.unique(), email, password);
+      const userId = ID.unique();
+      await account.create(userId, email, password, age);
       showMessage({
-        message: "Account Successfully Created!",
-        description: "Saved to Appwrite DB",
-        type: "success",
+        message: 'Account Successfully Created!',
+        description: 'Saved to Appwrite DB',
+        type: 'success',
       });
     } catch (err) {
       showMessage({
-        message: "Error",
+        message: 'Error',
         description: `${err}`,
-        type: "danger",
+        type: 'danger',
       });
     }
   }
 
   return (
     <>
-      <View style={styles.container}>
-        <Image
-          style={styles.image}
-          source={{
-            uri: "https://png.pngtree.com/element_our/20200610/ourlarge/pngtree-library-reading-student-image_2250158.jpg",
-          }}
-        />
-        <Text style={styles.heading}>Sign Up</Text>
-        <Text style={styles.discription}>Create an account to access amazing and great stories ahead!!!</Text>
+    <View style={styles.container}>
+      <Text style={styles.heading}>Sign Up</Text>
+      <Text style={styles.description}>
+        Create an account to access amazing and great stories ahead!!!
+      </Text>
 
+      <Text style={styles.header}>Email</Text>
+      <TextInput
+        placeholder="storybook@gmail.com"
+        value={email}
+        onChangeText={setEmail}
+        style={styles.input}
+        keyboardType="email-address"
+        autoCapitalize="none"
+        placeholderTextColor="gray"
+      />
+
+      <Text style={styles.header}>Password</Text>
+      <TextInput
+        placeholder="*********"
+        value={password}
+        onChangeText={setPassword}
+        style={styles.input}
+        secureTextEntry
+        placeholderTextColor="gray"
+      />
+
+      <Text style={styles.header}>Age </Text>
+      <TextInput
+        placeholder="Enter your age"
+        style={styles.input}
+        onChangeText={(text) => setAge(text)}
+        value={age}
+        placeholderTextColor="gray"
+      />
+
+      <Text style={styles.header}>Bio </Text>
+      <TextInput
+        placeholder="Write a short bio"
+        style={styles.input}
+        onChangeText={(text) => setBio(text)}
+        value={bio}
+        placeholderTextColor="gray"
+      />
+
+<Text style={styles.header}>First Name</Text>
         <TextInput
-          placeholder="Email"
-          value={email}
-          onChangeText={setEmail}
+        placeholder=" Jose"
           style={styles.input}
-          keyboardType="email-address"
-          autoCapitalize="none"
+          onChangeText={(text) => setFirstName(text)}
+          value={firstname}
+          placeholderTextColor="gray"
         />
 
+        <Text style={styles.header}>Middle Name</Text>
+        <TextInput 
+        placeholder="Andrew"
+        style={styles.input}
+        onChangeText={(text)=>setMiddleName(text)}
+        value={middlename}
+        placeholderTextColor="gray"
+        />
+
+        <Text style={styles.header}>Last Name</Text>
         <TextInput
-          placeholder="Password"
-          value={password}
-          onChangeText={setPassword}
+        placeholder="Dayne"
           style={styles.input}
-          secureTextEntry
+          onChangeText={(text) => setLastName(text)}
+          value={lastname}
+          placeholderTextColor="gray"
         />
 
-        <Pressable style={styles.button} onPress={handleSignup}>
-          <Text style={styles.buttonText}>Sign Up</Text>
-        </Pressable>
-      </View>
+        <Text style={styles.header}>Interests</Text>
+        <TextInput
+        placeholder="Reading"
+          style={styles.input}
+          onChangeText={(text) => setInterests(text)}
+          value={interests}
+          placeholderTextColor="gray"
+        />
+
+        <Text style={styles.header}>Favorite Genres</Text>
+        <TextInput
+        placeholder="Action"
+          style={styles.input}
+          onChangeText={(text) => setFavoriteGenres(text)}
+          value={favoriteGenres}
+          placeholderTextColor="gray"
+        />
+
+      
+
+
+      
+
+      <Pressable style={styles.button} onPress={handleSignup}>
+        <Text style={styles.buttonText}>Sign Up</Text>
+      </Pressable>
 
       <View
         style={{
           paddingBottom: 20,
-          backgroundColor: "#f5f5f5",
-          justifyContent: "center",
-          alignItems: "center",
+          justifyContent: 'center',
+          alignItems: 'center',
         }}
       >
-        <TouchableOpacity onPress={() => navigation.navigate("Login")}>
-          <Text style={{ fontSize: 15, fontWeight: "500" }}>
-            Already have an account? <Text style={{ color: '#3498db' }}>Login</Text>
+        <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+          <Text style={{ fontSize: 30, fontWeight: '500' }}>
+            Already have an account?{' '}
+            <Text style={{ color: '#012F31' }}>Login</Text>
           </Text>
         </TouchableOpacity>
       </View>
-      <FlashMessage position="top" />
 
+      <FlashMessage position="top" />
+    </View>
     </>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#f5f5f5',
+    backgroundColor: '#01757A',
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    padding: 16,
+    padding: 30,
   },
   image: {
     height: 160,
     width: 200,
   },
   heading: {
-    fontSize: 25,
-    fontWeight: "500",
+    fontSize: 50,
+    fontWeight: "bold",
     marginBottom: 2,
   },
-  discription: {
+
+  header:{
+    textAlign:"center",
+    fontSize:30,
+
+
+  },
+  description: {
     textAlign: "center",
     fontWeight: "300",
-    fontSize: 13,
+    fontFamily:"Monteserrat-Regular",
+    fontSize: 25,
     width: "70%",
     marginBottom: 50,
   },
   input: {
     height: 40,
     borderColor: "gray",
+    backgroundColor:"#D5FBFE",
     borderWidth: 0,
     borderBottomWidth: 1,
     marginBottom: 16,
-    width: "100%",
+    width: "70%",
+    borderRadius: 5,
     padding: 8,
-    textAlign: 'center',
+    textAlign: "center",
+    fontSize:20,
   },
   button: {
-    backgroundColor: "#3498db",
+    backgroundColor: "#0A3D40",
     padding: 10,
-    borderRadius: 5,  
-    width: "100%",
+    borderRadius: 5,
+    width: "40%",
     alignItems: "center",
-},
-buttonText: {
+  },
+  buttonText: {
     color: "white",
     fontSize: 16,
-},
+  },
 });
 
 export default Signup;
